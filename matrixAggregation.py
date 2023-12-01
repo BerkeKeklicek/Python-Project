3f7649e4a77c67b572e80f057fb424e8be27cb6e
Berke Keklicek <149376908+BerkeKeklicek@users.noreply.github.com>
2023-12-01 17:30:36 +0300

    matrixAggregation.py

--- matrixAggregation.py
+++ matrixAggregation.py
@@ -2,10 +2,10 @@
 x = [[1,3,5],[2,4,1],[1,5,7]]
 y = [[3,3,4],[2,4,1],[3,5,4]]
 
-sonuc = [[0,0,0],[0,0,0],[0,0,0]]
+result = [[0,0,0],[0,0,0],[0,0,0]]
 
 for i in range(len(x)):
     for j in range(len(y)):
-        sonuc[i][j] = x[i][j]+y[i][j]
+        result[i][j] = x[i][j]+y[i][j]
     
-print(sonuc)        
+print(result)        
